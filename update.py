13b2156e286a2d696ad434d15b6682ec259689fd
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-10-24 11:06:42 +0100

    more

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 24 04:23:10 2023
+Created on Tue Oct 24 2023
 
 @author: Ayomikun
 """
@@ -12,13 +12,13 @@ import seaborn as sns
 # 1. Load the CSV data
 data = pd.read_csv('test.csv')
 
-# 2. Extensive Data Cleaning
+# 2.Data Cleaning
 # Drop rows with missing values
 data.dropna(subset=['Event_Type', 'Property_Cost'], inplace=True)
 
-# Function to Convert "k" to "1000" and "m" to "1000000"
+# Define a function to transform values like '1.3k' to 13000 and '1.3m' to 13000000
 def transform_property_cost(value):
-    value = str(value)  
+    value = str(value)  # Ensure it's a string
     if 'k' in value:
         return float(value.replace('k', '')) * 1000
     elif 'm' in value:
@@ -31,22 +31,29 @@ data['Property_Cost'] = data['Property_Cost'].apply(transform_property_cost)
 
 # 3. Basic Descriptive Statistics
 property_cost_stats = data['Property_Cost'].describe()
+mean = data['Property_Cost'].mean()
+median = data['Property_Cost'].median()
+mode = data['Property_Cost'].mode().iloc[0]
+std_deviation = data['Property_Cost'].std()
 
 # 4. Construct Graphical Summary
-# Create a histogram for Property_Cost
-plt.hist(data['Property_Cost'], bins=20)
-plt.title('Property Cost Distribution')
-plt.xlabel('Property Cost')
-plt.ylabel('Frequency')
+# Create a line graph for Event_Type vs. Property_Cost
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=data, x='Event_Type', y='Property_Cost', ci=None)
+plt.xticks(rotation=45)
+plt.xlabel('Event Type')
+plt.ylabel('Property Cost')
+plt.title('Event Type vs. Property Cost (Line Graph)')
 plt.show()
 
 # Create a bar plot for Event_Type
-plt.subplot(122)
-event_type_counts = data['Event_Type'].value_counts()
-event_type_counts.plot(kind='bar')
-plt.title('Event Type Distribution')
+plt.figure(figsize=(10, 6))
+sns.barplot(data=data, x='Event_Type', y='Property_Cost', ci=None)
+plt.xticks(rotation=45)
 plt.xlabel('Event Type')
-plt.ylabel('Count')
+plt.ylabel('Property Cost')
+plt.title('Event Type vs. Property Cost (Bar Plot)')
+plt.show()
 
 # Detailed plot
 plt.figure(figsize=(12, 6))
@@ -55,12 +62,6 @@ plt.xticks(rotation=45)
 plt.xlabel('Event Type')
 plt.ylabel('Count')
 plt.title('Event Type Distribution Detailed')
-plt.show()
-
-plt.figure(figsize=(10, 6))
-sns.histplot(data['Property_Cost'], kde=True)
-plt.xlabel('Property Cost')
-plt.title('Property Cost Distribution Detailed')
 plt.show()
 
 # 5. Prepare a Brief Summary Report
@@ -73,25 +74,17 @@ with open('summary_report.txt', 'w') as report_file:
     report_file.write("--- Event_Type Summary ---\n")
     report_file.write(str(event_type_summary) + "\n\n")
     report_file.write("--- Property_Cost Summary ---\n")
-    report_file.write(str(property_cost_summary) + "\n")
+    report_file.write(str(property_cost_summary) + "\n\n")
+    report_file.write("--- Mean ---\n")
+    report_file.write(f"Mean: {mean}\n")
+    report_file.write("--- Median ---\n")
+    report_file.write(f"Median: {median}\n")
+    report_file.write("--- Mode ---\n")
+    report_file.write(f"Mode: {mode}\n")
+    report_file.write("--- Standard Deviation ---\n")
+    report_file.write(f"Standard Deviation: {std_deviation}\n")
 
 # 7. Save the cleaned data to a new CSV with only 'Event_Type' and 'Property_Cost' columns
 data[['Event_Type', 'Property_Cost']].to_csv('new.csv', index=False)
-
-# 8. Plot 'Event_Type' vs. 'Property_Cost' - Histogram
-plt.figure(figsize=(12, 6))
-sns.histplot(data, x='Event_Type', y='Property_Cost')
-plt.xticks(rotation=45)
-plt.xlabel('Event Type')
-plt.ylabel('Property Cost')
-plt.title('Event Type vs. Property Cost (Histogram)')
-plt.show()
-
-# 9. Plot 'Event_Type' vs. 'Property_Cost' - Pie Chart
-plt.figure(figsize=(10, 6))
-event_type_counts = data['Event_Type'].value_counts()
-plt.pie(event_type_counts, labels=event_type_counts.index, autopct='%1.1f%%')
-plt.title('Event Type Distribution (Pie Chart)')
-plt.show()
 
 print("Summary report saved as 'summary_report.txt', your cleaned CSV file is saved as 'cleaned_data.csv', and the cleaned data with only 'Event_Type' and 'Property_Cost' columns is exported to 'new.csv'.")
